6984d6e689d4e8a58b7716b67266238477c7eecc
Prateek Mohapatra <prateekmsoa@gmail.com>
2025-02-18 13:42:55 +0530

    optimised main.jsx + removed gif from home screen [adhd trap]

--- src/main.jsx
+++ src/main.jsx
@@ -1,54 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ClerkProvider } from '@clerk/clerk-react';
-import Landing from './pages/Landing';
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import InfoPage from './pages/InfoPage';
-import PredictionPage from './pages/PredictionPage';
-import ChatPage from './pages/ChatPage';
-import ContactPage from './pages/ContactPage';
-import ProtectedRoute from './components/ProtectedRoute';
+import App from './App';
 import './index.css';
-
-const CLERK_PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_KEY;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/InfoPage" element={<InfoPage />} />
-          <Route path="/AboutPage" element={<AboutPage />} />
-          <Route path="/ContactPage" element={<ContactPage />} />
-          <Route
-            path="/HomePage"
-            element={
-              <ProtectedRoute>
-                <HomePage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/ChatPage"
-            element={
-              <ProtectedRoute>
-                <ChatPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/PredictionPage"
-            element={
-              <ProtectedRoute>
-                <PredictionPage />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </Router>
-    </ClerkProvider>
+      <App />
   </React.StrictMode>
 );
